aec46405befc12aa3199e52365d167384977f918
Seungjae Lee <seungjae2525@gmail.com>
2022-05-26 22:40:39 +0900

    Update package

--- R/plot.RMSTSens.R
+++ R/plot.RMSTSens.R
@@ -95,7 +95,7 @@ autoplot.RMSTSens <- function(x, smooth.degree=11, alpha.ci=0.9, alpha.range=0.4
   xx <- x$result.df
 
   if (length(xx$Lambda) == 1) {
-    stop("\n Error: To plot the results, \"lambda\" must be a vector.")
+    stop("\n Error: To plot the sensitivity analysis results, \"lambda\" must be a vector.")
   }
 
   Lambda <- RMST.diff.min.lower <- RMST.diff.max.upper <- RMST.diff.min <- RMST.diff.max <- NULL
@@ -103,85 +103,164 @@ autoplot.RMSTSens <- function(x, smooth.degree=11, alpha.ci=0.9, alpha.range=0.4
     ylabel <- seq(floor(min(xx$RMST.diff.min.lower)/yscale)*yscale,
                   ceiling(max(xx$RMST.diff.max.upper)/yscale)*yscale, ytickdiff)
 
-    g1 <- ggplot(xx) +
-      stat_smooth(aes(x=Lambda, y=RMST.diff.min.lower, colour = "min"),
-                  method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree) +
-      stat_smooth(aes(x=Lambda, y=RMST.diff.max.upper, colour = "max"),
-                  method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree)
-    gg1 <- ggplot_build(g1)
+    if(length(xx$Lambda) == 2){
+      x0 <- seq(xx$Lambda[1], xx$Lambda[2], by = 0.01)
 
-    g2 <- ggplot(xx) +
-      stat_smooth(aes(x=Lambda, y=RMST.diff.min, colour = "min"),
-                  method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree) +
-      stat_smooth(aes(x=Lambda, y=RMST.diff.max, colour = "max"),
-                  method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree)
-    gg2 <- ggplot_build(g2)
+      aa1 <- (xx$RMST.diff.min.lower[2] - xx$RMST.diff.min.lower[1])/(xx$Lambda[2] - xx$Lambda[1])^2
+      f1 <- function(x) -aa1*(x-xx$Lambda[2])^2 + xx$RMST.diff.min.lower[2]
 
-    df <- data.frame(Lambda = gg1$data[[1]]$x,
-                     RMST.diff.min.lower = gg1$data[[1]]$y,
-                     RMST.diff.max.upper = gg1$data[[2]]$y,
-                     RMST.diff.min = gg2$data[[1]]$y,
-                     RMST.diff.max = gg2$data[[2]]$y)
-    df <- rbind(df, xx[,c("Lambda","RMST.diff.min.lower","RMST.diff.max.upper",
-                          "RMST.diff.min","RMST.diff.max")])
-    df <- df[order(df$Lambda),]
-    df <- df[!duplicated(round(df, 10), fromLast = TRUE),]
+      aa2 <- (xx$RMST.diff.min[2] - xx$RMST.diff.min[1])/(xx$Lambda[2] - xx$Lambda[1])^2
+      f2 <- function(x) -aa2*(x-xx$Lambda[2])^2 + xx$RMST.diff.min[2]
 
-    g <- ggplot(df) +
-      geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min.lower, ymax=RMST.diff.max.upper),
-                  alpha=alpha.ci, inherit.aes=F, fill="pink") +
-      geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min, ymax=RMST.diff.max),
-                  alpha=alpha.range, inherit.aes=F, fill="red") +
-      geom_line(aes(x=Lambda, y=xx$RMST.diff.min[1]), colour="red", size=h.width) +
-      geom_point(data=xx, aes(x=Lambda, y=RMST.diff.min), size=point.size, colour="blue") +
-      geom_point(data=xx, aes(x=Lambda, y=RMST.diff.max), size=point.size, colour="blue") +
-      geom_line(aes(x=Lambda, y=0), colour="black", size=h.width, linetype = "dashed", alpha=0.5) +
-      xlab(expression(Lambda)) + ylab("Between-group difference in bias-adjusted RMST")  +
-      theme_bw() +
-      scale_x_continuous(breaks = xx$Lambda,
-                         labels = sprintf("%.1f", xx$Lambda), expand = c(0.005,0.005)) +
-      scale_y_continuous(breaks = ylabel, labels = ylabel) +
-      theme(axis.title.x=element_text(face="bold", size=axis.title.size),
-            axis.title.y=element_text(face="bold", size=axis.title.size, angle=90),
-            axis.text=element_text(face="bold", size=axis.text.size),
-            panel.grid.minor.x = element_blank(),
-            panel.grid.minor.y = element_blank())
+      aa3 <- (xx$RMST.diff.max.upper[2] - xx$RMST.diff.max.upper[1])/(xx$Lambda[2] - xx$Lambda[1])^2
+      f3 <- function(x) -aa3*(x-xx$Lambda[2])^2 + xx$RMST.diff.max.upper[2]
 
+      aa4 <- (xx$RMST.diff.max[2] - xx$RMST.diff.max[1])/(xx$Lambda[2] - xx$Lambda[1])^2
+      f4 <- function(x) -aa4*(x-xx$Lambda[2])^2 + xx$RMST.diff.max[2]
+
+      df <- data.frame(Lambda = x0,
+                       RMST.diff.min.lower = f1(x = x0),
+                       RMST.diff.max.upper = f3(x = x0),
+                       RMST.diff.min = f2(x = x0),
+                       RMST.diff.max = f4(x = x0))
+      df <- rbind(df, xx[,c("Lambda","RMST.diff.min.lower","RMST.diff.max.upper",
+                            "RMST.diff.min","RMST.diff.max")])
+
+      g <- ggplot(df) +
+        geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min.lower, ymax=RMST.diff.max.upper),
+                    alpha=alpha.ci, inherit.aes=F, fill="pink") +
+        geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min, ymax=RMST.diff.max),
+                    alpha=alpha.range, inherit.aes=F, fill="red") +
+        geom_line(aes(x=Lambda, y=xx$RMST.diff.min[1]), colour="red", size=h.width) +
+        geom_point(data=xx, aes(x=Lambda, y=RMST.diff.min), size=point.size, colour="blue") +
+        geom_point(data=xx, aes(x=Lambda, y=RMST.diff.max), size=point.size, colour="blue") +
+        geom_line(aes(x=Lambda, y=0), colour="black", size=h.width, linetype = "dashed", alpha=0.5) +
+        xlab(expression(Lambda)) + ylab("Between-group difference in bias-adjusted RMST")  +
+        theme_bw() +
+        scale_x_continuous(breaks = xx$Lambda,
+                           labels = sprintf("%.1f", xx$Lambda), expand = c(0.005,0.005)) +
+        scale_y_continuous(breaks = ylabel, labels = ylabel) +
+        theme(axis.title.x=element_text(face="bold", size=axis.title.size),
+              axis.title.y=element_text(face="bold", size=axis.title.size, angle=90),
+              axis.text=element_text(face="bold", size=axis.text.size),
+              panel.grid.minor.x = element_blank(),
+              panel.grid.minor.y = element_blank())
+
+    } else {
+      g1 <- ggplot(xx) +
+        stat_smooth(aes(x=Lambda, y=RMST.diff.min.lower, colour = "min"),
+                    method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree) +
+        stat_smooth(aes(x=Lambda, y=RMST.diff.max.upper, colour = "max"),
+                    method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree)
+      gg1 <- ggplot_build(g1)
+
+      g2 <- ggplot(xx) +
+        stat_smooth(aes(x=Lambda, y=RMST.diff.min, colour = "min"),
+                    method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree) +
+        stat_smooth(aes(x=Lambda, y=RMST.diff.max, colour = "max"),
+                    method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree)
+      gg2 <- ggplot_build(g2)
+
+      df <- data.frame(Lambda = gg1$data[[1]]$x,
+                       RMST.diff.min.lower = gg1$data[[1]]$y,
+                       RMST.diff.max.upper = gg1$data[[2]]$y,
+                       RMST.diff.min = gg2$data[[1]]$y,
+                       RMST.diff.max = gg2$data[[2]]$y)
+      df <- rbind(df, xx[,c("Lambda","RMST.diff.min.lower","RMST.diff.max.upper",
+                            "RMST.diff.min","RMST.diff.max")])
+      df <- df[order(df$Lambda),]
+      df <- df[!duplicated(round(df, 10), fromLast = TRUE),]
+
+      g <- ggplot(df) +
+        geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min.lower, ymax=RMST.diff.max.upper),
+                    alpha=alpha.ci, inherit.aes=F, fill="pink") +
+        geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min, ymax=RMST.diff.max),
+                    alpha=alpha.range, inherit.aes=F, fill="red") +
+        geom_line(aes(x=Lambda, y=xx$RMST.diff.min[1]), colour="red", size=h.width) +
+        geom_point(data=xx, aes(x=Lambda, y=RMST.diff.min), size=point.size, colour="blue") +
+        geom_point(data=xx, aes(x=Lambda, y=RMST.diff.max), size=point.size, colour="blue") +
+        geom_line(aes(x=Lambda, y=0), colour="black", size=h.width, linetype = "dashed", alpha=0.5) +
+        xlab(expression(Lambda)) + ylab("Between-group difference in bias-adjusted RMST")  +
+        theme_bw() +
+        scale_x_continuous(breaks = xx$Lambda,
+                           labels = sprintf("%.1f", xx$Lambda), expand = c(0.005,0.005)) +
+        scale_y_continuous(breaks = ylabel, labels = ylabel) +
+        theme(axis.title.x=element_text(face="bold", size=axis.title.size),
+              axis.title.y=element_text(face="bold", size=axis.title.size, angle=90),
+              axis.text=element_text(face="bold", size=axis.text.size),
+              panel.grid.minor.x = element_blank(),
+              panel.grid.minor.y = element_blank())
+
+    }
   } else {
     ylabel <- seq(floor(min(xx$RMST.diff.min)/yscale)*yscale,
                   ceiling(max(xx$RMST.diff.max)/yscale)*yscale, ytickdiff)
 
-    g2 <- ggplot(xx) +
-      stat_smooth(aes(x=Lambda, y=RMST.diff.min, colour = "min"),
-                  method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree) +
-      stat_smooth(aes(x=Lambda, y=RMST.diff.max, colour = "max"),
-                  method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree)
-    gg2 <- ggplot_build(g2)
+    if(length(xx$Lambda) == 2){
+      x0 <- seq(xx$Lambda[1], xx$Lambda[2], by = 0.01)
 
-    df <- data.frame(Lambda = gg2$data[[1]]$x,
-                     RMST.diff.min = gg2$data[[1]]$y,
-                     RMST.diff.max = gg2$data[[2]]$y)
-    df <- rbind(df, xx[,c("Lambda","RMST.diff.min","RMST.diff.max")])
-    df <- df[order(df$Lambda),]
-    df <- df[!duplicated(round(df, 10), fromLast = TRUE),]
+      aa2 <- (xx$RMST.diff.min[2] - xx$RMST.diff.min[1])/(xx$Lambda[2] - xx$Lambda[1])^2
+      f2 <- function(x) -aa2*(x-xx$Lambda[2])^2 + xx$RMST.diff.min[2]
 
-    g <- ggplot(df) +
-      geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min, ymax=RMST.diff.max),
-                  alpha=alpha.range, inherit.aes=F, fill="red") +
-      geom_line(aes(x=Lambda, y=xx$RMST.diff.min[1]), colour="red", size=h.width) +
-      geom_point(data=xx, aes(x=Lambda, y=RMST.diff.min), size=point.size, colour="blue") +
-      geom_point(data=xx, aes(x=Lambda, y=RMST.diff.max), size=point.size, colour="blue") +
-      geom_line(aes(x=Lambda, y=0), colour="black", size=h.width, linetype = "dashed", alpha=0.5) +
-      xlab(expression(Lambda)) + ylab("Between-group difference in bias-adjusted RMST")  +
-      theme_bw() +
-      scale_x_continuous(breaks = xx$Lambda,
-                         labels = sprintf("%.1f", xx$Lambda), expand = c(0.005,0.005)) +
-      scale_y_continuous(breaks = ylabel, labels = ylabel) +
-      theme(axis.title.x=element_text(face="bold", size=axis.title.size),
-            axis.title.y=element_text(face="bold", size=axis.title.size, angle=90),
-            axis.text=element_text(face="bold", size=axis.text.size),
-            panel.grid.minor.x = element_blank(),
-            panel.grid.minor.y = element_blank())
+      aa4 <- (xx$RMST.diff.max[2] - xx$RMST.diff.max[1])/(xx$Lambda[2] - xx$Lambda[1])^2
+      f4 <- function(x) -aa4*(x-xx$Lambda[2])^2 + xx$RMST.diff.max[2]
+
+      df <- data.frame(Lambda = x0,
+                       RMST.diff.min = f2(x = x0),
+                       RMST.diff.max = f4(x = x0))
+      df <- rbind(df, xx[,c("Lambda","RMST.diff.min","RMST.diff.max")])
+
+      g <- ggplot(df) +
+        geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min, ymax=RMST.diff.max),
+                    alpha=alpha.range, inherit.aes=F, fill="red") +
+        geom_line(aes(x=Lambda, y=xx$RMST.diff.min[1]), colour="red", size=h.width) +
+        geom_point(data=xx, aes(x=Lambda, y=RMST.diff.min), size=point.size, colour="blue") +
+        geom_point(data=xx, aes(x=Lambda, y=RMST.diff.max), size=point.size, colour="blue") +
+        geom_line(aes(x=Lambda, y=0), colour="black", size=h.width, linetype = "dashed", alpha=0.5) +
+        xlab(expression(Lambda)) + ylab("Between-group difference in bias-adjusted RMST")  +
+        theme_bw() +
+        scale_x_continuous(breaks = xx$Lambda,
+                           labels = sprintf("%.1f", xx$Lambda), expand = c(0.005,0.005)) +
+        scale_y_continuous(breaks = ylabel, labels = ylabel) +
+        theme(axis.title.x=element_text(face="bold", size=axis.title.size),
+              axis.title.y=element_text(face="bold", size=axis.title.size, angle=90),
+              axis.text=element_text(face="bold", size=axis.text.size),
+              panel.grid.minor.x = element_blank(),
+              panel.grid.minor.y = element_blank())
+    } else {
+
+      g2 <- ggplot(xx) +
+        stat_smooth(aes(x=Lambda, y=RMST.diff.min, colour = "min"),
+                    method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree) +
+        stat_smooth(aes(x=Lambda, y=RMST.diff.max, colour = "max"),
+                    method = "glm", formula = y ~ splines::ns(x,2), n=smooth.degree)
+      gg2 <- ggplot_build(g2)
+
+      df <- data.frame(Lambda = gg2$data[[1]]$x,
+                       RMST.diff.min = gg2$data[[1]]$y,
+                       RMST.diff.max = gg2$data[[2]]$y)
+      df <- rbind(df, xx[,c("Lambda","RMST.diff.min","RMST.diff.max")])
+      df <- df[order(df$Lambda),]
+      df <- df[!duplicated(round(df, 10), fromLast = TRUE),]
+
+      g <- ggplot(df) +
+        geom_ribbon(aes(x=Lambda, ymin=RMST.diff.min, ymax=RMST.diff.max),
+                    alpha=alpha.range, inherit.aes=F, fill="red") +
+        geom_line(aes(x=Lambda, y=xx$RMST.diff.min[1]), colour="red", size=h.width) +
+        geom_point(data=xx, aes(x=Lambda, y=RMST.diff.min), size=point.size, colour="blue") +
+        geom_point(data=xx, aes(x=Lambda, y=RMST.diff.max), size=point.size, colour="blue") +
+        geom_line(aes(x=Lambda, y=0), colour="black", size=h.width, linetype = "dashed", alpha=0.5) +
+        xlab(expression(Lambda)) + ylab("Between-group difference in bias-adjusted RMST")  +
+        theme_bw() +
+        scale_x_continuous(breaks = xx$Lambda,
+                           labels = sprintf("%.1f", xx$Lambda), expand = c(0.005,0.005)) +
+        scale_y_continuous(breaks = ylabel, labels = ylabel) +
+        theme(axis.title.x=element_text(face="bold", size=axis.title.size),
+              axis.title.y=element_text(face="bold", size=axis.title.size, angle=90),
+              axis.text=element_text(face="bold", size=axis.text.size),
+              panel.grid.minor.x = element_blank(),
+              panel.grid.minor.y = element_blank())
+    }
   }
 
   ##
